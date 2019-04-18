1e1c3f344b71e4bc23ab71fff0af382f7552c08e
damianbartumeu <damian.bartumeu@logangroupservices.com>
2019-04-18 15:32:38 -0400

    feat(*): Adding types definitions

---
+++ src/lib/Flex.d.ts
@@ -0,0 +1,63 @@
+import * as React from 'react';
+
+export type JustifyContent =
+  | 'start'
+  | 'end'
+  | 'center'
+  | 'space-between'
+  | 'space-between'
+  | 'space-evenly';
+
+export type AlignItems = 'start' | 'end' | 'center' | 'baseline' | 'stretch';
+
+export type Width = string | number;
+
+export type Height = string | number;
+
+export interface FlexProps {
+  children: React.Element<any>;
+  className?: string;
+  inline?: boolean | { [key: string]: boolean };
+  direction?: string | { [key: string]: string };
+  justifyContent?: JustifyContent | { [key: string]: JustifyContent };
+  alignItems?: AlignItems | { [key: string]: AlignItems };
+  alignSelf?: 'auto' | AlignItems | { [key: string]: 'auto' | AlignItems };
+  alignContent?: AlignItems | { [key: string]: AlignItems };
+  wrap?: boolean | { [key: string]: boolean };
+  grow?: number | { [key: string]: number };
+  shrink?: number | { [key: string]: number };
+  basis?: string | number | { [key: string]: string | number };
+  spacing?: string | { [key: string]: string };
+  gap?: number | { [key: string]: number };
+  layoutGap?: number;
+  fill?: boolean | { [key: string]: boolean };
+  align?:
+    | (JustifyContent | AlignItems)
+    | [JustifyContent, AlignItems]
+    | {
+        [key: string]:
+          | (JustifyContent | AlignItems)
+          | [JustifyContent, AlignItems];
+      };
+  size?:
+    | string
+    | number
+    | [Width, Height]
+    | { [key: string]: string | number | [Width, Height] };
+  minSize?:
+    | string
+    | number
+    | [Width, Height]
+    | { [key: string]: string | number | [Width, Height] };
+  maxSize?:
+    | string
+    | number
+    | [Width, Height]
+    | { [key: string]: string | number | [Width, Height] };
+  show?: boolean | { [key: string]: boolean };
+  hide?: boolean | { [key: string]: boolean };
+}
+
+declare var Flex: React.ComponentType<FlexProps>;
+
+export default Flex;
